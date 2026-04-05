4920162576c927b9e9a209fffff43e7c99d0cc7d
Bruno-DarkSouler <brunofornasar@gmail.com>
2026-04-05 00:22:55 -0300

    Cambio de interfaz

--- js/mostrar_datos.js
+++ js/mostrar_datos.js
@@ -1,92 +1,52 @@
 const coto = document.getElementById("coto");
 const jumbo = document.getElementById("jumbo");
 const dia = document.getElementById("dia");
+let datos_coto;
+let datos_dia;
+let datos_jumbo;
+let datos_generales;
+const lista_productos = document.getElementById("lista_productos");
+let contenido = "";
+const barra_busqueda = document.getElementById("barra_busqueda");
 
-function cargarDatosCoto(){
+function cargarDatosCoto(callback){
     let ajax = new XMLHttpRequest();
     ajax.onload = function(){
         let datos = JSON.parse(ajax.responseText);
-        datos = ordenarPrecio(datos);
-        console.log(datos);
-        let contenido_contenedor = ``;
-        datos.forEach(producto => {
-            contenido_contenedor = contenido_contenedor + `
-            <div class="producto">
-                <div class="contenedor_imagen">
-                    <img src="${producto.imagen}" alt="Imagen producto">
-                </div>
-                <div class="oferta">
-                <span>
-                ${producto.oferta}
-                </span>
-                </div>
-                <div class="contenedor_info">
-                    <p>${producto.nombre}</p> 
-                    <span>Precio por 100g: \$${producto.precio}</span>
-                </div>
-            </div>
-            `;
-        });
-        coto.innerHTML = contenido_contenedor
+        for(let i = 0; i < datos.length; i++){
+            datos[i].mercado = "coto";
+        }
+        callback(datos);
     }
     ajax.open("GET", "./datos/datos_coto.json", "true");
     ajax.send();
 }
 
-function cargarDatosJumbo(){
+function cargarDatosJumbo(callback){
     let ajax = new XMLHttpRequest();
     ajax.onload = function(){
         let datos = JSON.parse(ajax.responseText);
-        datos = ordenarPrecio(datos);
-        // console.log(datos);
-        let contenido_contenedor = ``;
-        datos.forEach(producto => {
-            if(producto.nombre != null){
-                contenido_contenedor = contenido_contenedor + `
-                <div class="producto">
-                    <div class="contenedor_imagen">
-                        <img src="${producto.imagen}" alt="Imagen producto">
-                    </div>
-                    <div class="contenedor_info">
-                        <p>${producto.nombre}</p> 
-                        <span>Precio por 100g: \$${producto.precio}</span>
-                    </div>
-                </div>
-                `;
-            }
-        });
-        jumbo.innerHTML = contenido_contenedor
+        for(let i = 0; i < datos.length; i++){
+            datos[i].mercado = "jumbo";
+        }
+        callback(datos);
+        datos_generales = [...datos_coto, ...datos_dia, ...datos_jumbo];
+
+    crear_productos(ordenarPrecio(datos_generales));
+        console.log(ordenarPrecio(datos_generales));
     }
     ajax.open("GET", "./datos/datos_jumbo.json", "true");
     ajax.send();
 }
 
-function cargarDatosDia(){
+function cargarDatosDia(callback){
     let ajax = new XMLHttpRequest();
     ajax.onload = function(){
         let datos = JSON.parse(ajax.responseText);
-        datos = ordenarPrecio(datos);
-        // console.log(datos);
-        let contenido_contenedor = ``;
-        datos.forEach(producto => {
-            contenido_contenedor = contenido_contenedor + `
-            <div class="producto">
-                <div class="contenedor_imagen">
-                    <img src="${producto.imagen}" alt="Imagen producto">
-                </div>
-                <div class="oferta">
-                <span>
-                ${producto.oferta}
-                </span>
-                </div>
-                <div class="contenedor_info">
-                    <p>${producto.nombre}</p> 
-                    <span>Precio por 100g: \$${producto.precio}</span>
-                </div>
-            </div>
-            `;
-        });
-        dia.innerHTML = contenido_contenedor
+        for(let i = 0; i < datos.length; i++){
+            datos[i].mercado = "dia";
+        }
+        callback(datos);
     }
     ajax.open("GET", "./datos/datos_dia.json", "true");
     ajax.send();
@@ -112,6 +72,127 @@ function ordenarPrecio(datos_desordenados){
     return datos_ordenados;
 }
 
-cargarDatosCoto();
-cargarDatosDia();
-cargarDatosJumbo();
+
+
+cargarDatosCoto(function(datos){
+    datos_coto = datos;
+});
+
+cargarDatosDia(function(datos){
+    datos_dia = datos;
+});
+
+cargarDatosJumbo(function(datos){
+    datos_jumbo = datos;
+});
+
+
+
+function crear_productos(datos_productos){
+    datos_productos.forEach(producto => {
+        if(producto.mercado == "dia"){
+            contenido += `
+            <div class="producto">
+                <div class="contenedor_img_producto">
+                    <img src="${producto.imagen}" alt="Imagen producto">
+                </div>
+                <div class="contenedor_nombre">
+                    ${producto.nombre}
+                </div>
+                <div class="contenedor_precio">
+                    <div>
+                        Precio x 100g: <br>
+                        <span>\$${Math.round(producto.precio)}</span>
+                    </div>
+                </div>
+                <div class="contenedor_oferta">
+                    <div>
+                        Oferta/Descuento: <br>
+                        <span>${producto.oferta}</span>
+                    </div>
+                </div>
+                <div class="contenedor_img_mercado">
+                    <img src="./img/dia.png" alt="Imagen supermercado">
+                </div>
+            </div>
+            `
+        }else{
+            if(producto.mercado == "jumbo"){
+                contenido += `
+                <div class="producto">
+                    <div class="contenedor_img_producto">
+                        <img src="${producto.imagen}" alt="Imagen producto">
+                    </div>
+                    <div class="contenedor_nombre">
+                        ${producto.nombre}
+                    </div>
+                    <div class="contenedor_precio">
+                        <div>
+                            Precio x 100g: <br>
+                            <span>\$${Math.round(producto.precio)}</span>
+                        </div>
+                    </div>
+                    <div class="contenedor_oferta">
+                        <div>
+                            Oferta/Descuento: <br>
+                            <span>Ninguna</span>
+                        </div>
+                    </div>
+                    <div class="contenedor_img_mercado">
+                        <img src="./img/jumbo.png" alt="Imagen supermercado">
+                    </div>
+                </div>
+                `
+            }else{
+                contenido += `
+                <div class="producto">
+                    <div class="contenedor_img_producto">
+                        <img src="${producto.imagen}" alt="Imagen producto">
+                    </div>
+                    <div class="contenedor_nombre">
+                        ${producto.nombre}
+                    </div>
+                    <div class="contenedor_precio">
+                        <div>
+                            Precio x 100g: <br>
+                            <span>\$${Math.round(producto.precio)}</span>
+                        </div>
+                    </div>
+                    <div class="contenedor_oferta">
+                        <div>
+                            Oferta/Descuento: <br>
+                            <span>${producto.oferta}</span>
+                        </div>
+                    </div>
+                    <div class="contenedor_img_mercado">
+                        <img src="./img/coto.jpg" alt="Imagen supermercado">
+                    </div>
+                </div>
+                `
+            }
+        }
+    });
+    
+    lista_productos.innerHTML = contenido;
+}
+
+function ocultarProductos(){
+    const productos = document.getElementsByClassName("producto");
+
+    for(let i = 0; i < productos.length; i++){
+        productos[i].classList.add("oculto");
+    }
+}
+
+function buscarProductos(){
+    const nombres = document.getElementsByClassName("contenedor_nombre");
+    ocultarProductos()
+
+    for(let i = 0; i < nombres.length; i++){
+        if(nombres[i].innerHTML.toLocaleLowerCase().includes(barra_busqueda.value.toLowerCase())){
+            nombres[i].parentElement.classList.remove("oculto");
+        }
+    }
+}
+
+barra_busqueda.onkeyup = buscarProductos;
